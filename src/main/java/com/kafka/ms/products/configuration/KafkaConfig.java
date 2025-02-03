71ec6ff73b640651a1d2793d4962830c3e2c527f
wenhaoliu <wliu1197@uni.sydney.edu.au>
2025-02-03 15:21:51 +1100

    Added code and description to use java configure Kafka producer acknowledgements

--- src/main/java/com/kafka/ms/products/configuration/KafkaConfig.java
+++ src/main/java/com/kafka/ms/products/configuration/KafkaConfig.java
@@ -1,15 +1,69 @@
 package com.kafka.ms.products.configuration;
 
 import com.kafka.ms.products.model.constants.Constants;
+import com.kafka.ms.products.model.event.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+/*
+    // This part of code using java to configure producer acks and creates KafkaTemplate spring bean from ProducerFactory
+    // for ProductCreatedEvent then we can inject this KafkaTemplate to the business logic
+
+    @Value("kafka.producer.bootstrap-servers")
+    private String bootstrapServers;
+    @Value("kafka.producer.key-serializer")
+    private String keySerializer;
+    @Value("kafka.producer.value-serializer")
+    private String valueSerializer;
+    @Value("spring.kafka.producer.acks")
+    private String acks;
+    @Value("spring.kafka.producer.retries")
+    private String retries;
+    @Value("kafka.producer.properties.retry.backoff.ms")
+    private String retryBackOff;
+    @Value("kafka.producer.properties.delivery.timeout.ms")
+    private String deliveryTimeout;
+    @Value("kafka.producer.properties.linger.ms")
+    private String linger;
+    @Value("kafka.producer.properties.request.timeout.ms")
+    private String requestTimeout;
+
+    Map<String,Object> producerConfigs() {
+        Map<String,Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG,acks);
+        config.put(ProducerConfig.RETRIES_CONFIG,retries);
+        config.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG,retryBackOff);
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG,deliveryTimeout);
+        config.put(ProducerConfig.LINGER_MS_CONFIG,linger);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,requestTimeout);
+        return config;
+    }
+
+    @Bean
+    ProducerFactory<String, ProductCreatedEvent> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+    @Bean
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate(){
+        return new KafkaTemplate<String, ProductCreatedEvent>(producerFactory());
+    }
+*/
+
     @Bean
     NewTopic createTopic(){
         // we can have 3 broker servers running so we can replicate messages to provide high availability
